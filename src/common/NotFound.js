87c11381f2a1a8f5a5b9b71f6704457974572707
touzet_t <theo.touzet@epitech.eu>
2019-03-26 16:37:31 +0100

    Added image for 404

--- src/common/NotFound.js
+++ src/common/NotFound.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 
+import dirdNotFoundImage from '../assets/dirdnotfound.jpg';
+
 function NotFound() {
     return (
       <div>
         <Typography variant="h4">
-          This is a 404 !
+          This is a 404!
         </Typography>
+          <img src={dirdNotFoundImage} alt="404 image" style={{width: "70vw"}} />
       </div>
     );
 }
