41813025a9967786454a2845a36643fde076f609
Shad <shad@chaz.pro>
2019-03-26 14:36:23 +0100

    fix lint?

--- src/common/UserCard.js
+++ src/common/UserCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
-import DefaultPicture from '../User.png';
+import DefaultPicture from '../DefaultUser.png';
 
 const UserCard = (props) => {
   const { handle, email, picture } = props;
